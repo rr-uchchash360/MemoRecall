80652aeb7f2a3263ed9746ab5b2a51c818ec36ad
Md. Rafiur Rahman <95489408+rr-uchchash360@users.noreply.github.com>
2023-12-29 20:16:59 +0600

    image shows T_T

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow } from '@material-ui/core';
+
+import memorecall from './images/memorecall.png'
 
 const App = () => {
     return (
-        <div>
-            <h1>Test</h1>
-        </div>
-    );
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography varaint="h2" aligh="center">
+                    MemoRecall
+                </Typography>
+                <img src={memorecall} alt="memorecall" height="300" />
+            </AppBar>
+        </Container>
+    )
 }
 
 export default App;
