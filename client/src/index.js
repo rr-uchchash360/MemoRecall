3e8b4914c12b12214a7783d68bda4df7679e472c
Md. Rafiur Rahman <95489408+rr-uchchash360@users.noreply.github.com>
2023-12-30 12:59:16 +0600

    Error fixed
    By the grace of Almighty, I fixed it!

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-// import { legacy_createStore as createStore} from 'redux';
 import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
 import reducers from './reducers';
 
 import App from './app';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)))
+// Custom middleware to handle async actions
+const asyncMiddleware = ({ dispatch }) => next => action => {
+    if (typeof action === 'function') {
+      return action(dispatch);
+    }
+    return next(action);
+  };
+
+  const store = createStore(
+    reducers,
+    compose(
+      applyMiddleware(asyncMiddleware),
+      // Add other middleware or enhancers here if needed
+    )
+  );
 
 ReactDom.render(
     <Provider store = { store }>
